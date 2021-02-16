4646c03c3f57bdb6566f684309aabe67a830c5de
Jarred Sumner <jarred@jarredsumner.com>
2021-02-15 22:14:33 -0800

    try this

--- Formula/git-peek.rb
+++ Formula/git-peek.rb
@@ -5,7 +5,7 @@ class GitPeek < Formula
   homepage "https://github.com/Jarred-Sumner/git-peek"
   url "https://github.com/Jarred-Sumner/git-peek/releases/download/1.3.10/git-peek-macOS"
   head "https://github.com/Jarred-Sumner/git-peek.git"
-  # sha256 "361a46c436f5db7f30263034f426fa949d2db882e789e8fe1720d3e94e8f7a08"
+  sha256 "81a64aec02baa30b06704c6139337428cde1aec7f604757d170e79705b6384a0"
   license "MIT"
 
   livecheck do
@@ -16,7 +16,10 @@ class GitPeek < Formula
 
   def install
     bin.install "git-peek-macOS" => "git-peek"
-    system "git-peek"
+  en
+
+  def post_install
+    system "git-peek --register --postinstall"
   end
 
   test do
