48da452c845e906d4f7bd03b9c4c611e986d8e84
Jarred Sumner <jarred@jarredsumner.com>
2021-02-15 18:30:54 -0800

    use binary

--- Formula/git-peek.rb
+++ Formula/git-peek.rb
@@ -3,8 +3,8 @@ require "language/node"
 class GitPeek < Formula
   desc "git repo to local editor instantly"
   homepage "https://github.com/Jarred-Sumner/git-peek"
-  url "https://registry.npmjs.org/@jarred/git-peek/-/git-peek-1.3.3.tgz"
-  sha256 "72fe7877b6a7f295a2c341e0a568da8c071d75beec04a22bb6cd9f30d0460d5b"
+  url "https://github.com/Jarred-Sumner/git-peek/releases/download/1.3.6/git-peek-macOS"
+  sha256 "361a46c436f5db7f30263034f426fa949d2db882e789e8fe1720d3e94e8f7a08"
   head "https://github.com/Jarred-Sumner/git-peek.git"
   license "MIT"
 
@@ -12,12 +12,11 @@ class GitPeek < Formula
     url :stable
   end
 
-  depends_on "node"
   depends_on "git"
 
   def install
-    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
-    bin.install_symlink Dir["#{libexec}/bin/*"]
+    bin.install "git-peek-macOS" => "git-peek"
+    system "git-peek -r"
   end
 
   test do
