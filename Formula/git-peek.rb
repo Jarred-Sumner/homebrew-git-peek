75967ac2170d838dd2a06b2712476f3234e39825
Jarred Sumner <jarred@jarredsumner.com>
2021-02-15 23:00:57 -0800

    cool

--- Formula/git-peek.rb
+++ Formula/git-peek.rb
@@ -19,7 +19,7 @@ class GitPeek < Formula
   end
 
   def post_install
-    system "git-peek --register --postinstall"
+    puts "To add 1-click \"Peek\" buttons to GitHub, run git peek -r and install the browser extension.\n->  https://github.com/Jarred-Sumner/1-click-from-github-to-editor"
   end
 
   test do
