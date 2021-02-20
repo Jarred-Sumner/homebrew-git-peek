645cdb5cbac54559893932852c1283b4cca2cda4
Jarred Sumner <jarred@jarredsumner.com>
2021-02-20 02:46:33 -0800

    bump

--- Formula/git-peek.rb
+++ Formula/git-peek.rb
@@ -3,9 +3,9 @@ require "language/node"
 class GitPeek < Formula
   desc "git repo to local editor instantly"
   homepage "https://github.com/Jarred-Sumner/git-peek"
-  url "https://github.com/Jarred-Sumner/git-peek/releases/download/1.3.17/git-peek-macOS"
+  url "https://github.com/Jarred-Sumner/git-peek/releases/download/1.3.18/git-peek-macOS"
   head "https://github.com/Jarred-Sumner/git-peek.git"
-  sha256 "4ecd7c61f5ca6b824c21c3d549297d6073210ed5d6aede6722ae3209e1adf983"
+  sha256 "ac1864514119fbe3ed458a108daa9e399ff770b45fae40fb12bf38fdaa6d57e8"
   license "MIT"
 
   livecheck do
